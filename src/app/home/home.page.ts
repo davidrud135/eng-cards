e8ede210c82eb6e67fa8f619b1ef18198d51b0fd
David RJ <david.rud135@gmail.com>
2019-06-25 18:38:49 +0300

    Added home page content

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,5 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
-
+  
 }
