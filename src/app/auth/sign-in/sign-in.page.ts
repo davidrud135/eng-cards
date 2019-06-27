3b3bce316b86bd03f70a6edd02449d28b4e1c323
David RJ <david.rud135@gmail.com>
2019-06-27 20:31:46 +0300

    Added sign in functionality including progress bar and error handling

--- src/app/auth/sign-in/sign-in.page.ts
+++ src/app/auth/sign-in/sign-in.page.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../auth.service';
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +9,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.page.scss'],
 })
 export class SignInPage implements OnInit {
+  isLoading: boolean = false;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private toastController: ToastController
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSignIn(form: NgForm) {
+    this.isLoading = true;
+    const {email, password} = form.value;
+    this.authService.signIn(email, password).subscribe(
+      resp => {
+        console.log(resp);
+        this.isLoading = false;
+      },
+      errMessage => {
+        this.isLoading = false;
+        this.presentToast(errMessage);
+      }
+    );
+    form.reset();
+  }
+
+  async presentToast(text: string) {
+    const toast = await this.toastController.create({
+      position: 'top',
+      color: 'danger',
+      message: text,
+      showCloseButton: true
+    });
+    toast.present();
   }
 
 }
