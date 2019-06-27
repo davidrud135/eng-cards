fb80e074ac07d161bfc67f4a41c83e4395718686
David RJ <david.rud135@gmail.com>
2019-06-27 20:30:11 +0300

    Added sign in and common error handler methods

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
 
@@ -9,7 +9,8 @@ interface AuthResponseData {
   email: string,
   refreshToken: string,
   expiresIn: string,
-  localId: string
+  localId: string,
+  registered?: boolean
 }
 
 @Injectable({providedIn: 'root'})
@@ -27,21 +28,40 @@ export class AuthService {
           returnSecureToken: true
         }
       )
-      .pipe(catchError(errorResp => {
-        let errorMessage = 'An unknown error occurred!';
-        if (!errorResp.error || !errorResp.error.error) {
-          return;
+      .pipe(catchError(this.handleAuthError));
+  }
+
+  signIn(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDPCOezFSIj07NO924GvlsLDIA-1doYz7s',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true
         }
-        switch (errorResp.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'User with this email already exists!';
-            break;
-          case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-            errorMessage = 'Detected unusual activity. Please, try again later.';
-            break;
-        };
-        return throwError(errorMessage);
-      }));
+      )
+      .pipe(catchError(this.handleAuthError));;
+  }
+
+  handleAuthError(errorResp: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occurred!';
+    if (!errorResp.error || !errorResp.error.error) {
+      return;
+    }
+    switch (errorResp.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'User with this email already exists.';
+        break;
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage = 'Detected unusual activity. Please, try again later.';
+        break;
+      case 'EMAIL_NOT_FOUND':
+      case 'INVALID_PASSWORD':
+        errorMessage = 'Wrong email or password.';
+        break;
+    };
+    return throwError(errorMessage);
   }
 
 }
