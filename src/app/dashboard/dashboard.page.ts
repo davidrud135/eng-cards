5ca9a90accbe26197dbb160de8ef9f8208ff7876
David RJ <david.rud135@gmail.com>
2019-07-18 16:32:08 +0300

    Improved dashboard page

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Observable } from 'rxjs';
+
+import { DBService } from './../shared/database.service';
+import { UnitModalComponent } from './unit-modal/unit-modal.component';
+import { Unit } from '../shared/models/unit.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +13,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage implements OnInit {
+  units$: Observable<Unit[]>;
+  isNewUnitFormDisplayable: boolean = false;
 
-  constructor() { }
+  constructor(
+    private  dbService: DBService,
+    private modalCtrl: ModalController
+  ) {}
 
   ngOnInit() {
+    this.units$ = this.dbService.getUnits();
+  }
+
+  displayNewUnitForm() {
+    this.isNewUnitFormDisplayable = true;
+  }
+
+  hideNewUnitForm() {
+    this.isNewUnitFormDisplayable = false;
+  }
+
+  async openUnitModal(unit: Unit) {
+    const modal = await this.modalCtrl.create({
+      component: UnitModalComponent,
+      componentProps: {unit}
+    });
+    return await modal.present();
+  }
+
+  onUnitCreate(newUnitForm: NgForm) {
+    const {title} = newUnitForm.value;
+    this.dbService.addUnit(title).then(() => {
+      this.hideNewUnitForm();
+    });
+  }
+
+  onUnitEdit(unit: Unit) {
   }
 
 }
