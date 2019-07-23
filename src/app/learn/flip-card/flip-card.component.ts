862cd5fd896b278d5eddce7e12bc5a120aef28a0
David RJ <david.rud135@gmail.com>
2019-07-23 15:50:02 +0300

    Fixed bug on mobile devices: flip card component now uses only CSS animation

--- src/app/learn/flip-card/flip-card.component.ts
+++ src/app/learn/flip-card/flip-card.component.ts
@@ -1,34 +1,21 @@
 import { Card } from './../../shared/models/card.model';
 import { Component, OnInit, Input } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-flip-card',
   templateUrl: './flip-card.component.html',
-  styleUrls: ['./flip-card.component.scss'],
-  animations: [
-    trigger('flipState', [
-      state('active', style({
-        transform: 'rotateY(-179.9deg)'
-      })),
-      state('inactive', style({
-        transform: 'rotateY(0)'
-      })),
-      transition('active => inactive', animate('500ms ease-out')),
-      transition('inactive => active', animate('500ms ease-in'))
-    ])  
-  ]
+  styleUrls: ['./flip-card.component.scss']
 })
 export class FlipCardComponent implements OnInit {
   @Input() card: Card;
-  flip: string = 'inactive';
+  isFlipped: boolean = false;
 
   constructor() { }
 
   ngOnInit() {}
 
   toggleFlip() {
-    this.flip = (this.flip == 'inactive') ? 'active' : 'inactive';
+    this.isFlipped = !this.isFlipped;
   }
 
 }
