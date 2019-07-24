e3c1b398d9da646fef3d08425683e4820eb5dac2
David RJ <david.rud135@gmail.com>
2019-07-24 18:24:45 +0300

    Added message on Dashboard page if user doesn't have any units.

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { ModalController } from "@ionic/angular";
+import { Observable } from "rxjs";
 
-import { DBService } from './../shared/database.service';
-import { UnitModalComponent } from './unit-modal/unit-modal.component';
-import { Unit } from '../shared/models/unit.model';
-import { NgForm } from '@angular/forms';
+import { DBService } from "./../shared/database.service";
+import { UnitModalComponent } from "./unit-modal/unit-modal.component";
+import { Unit } from "../shared/models/unit.model";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.page.html',
-  styleUrls: ['./dashboard.page.scss'],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.page.html",
+  styleUrls: ["./dashboard.page.scss"]
 })
 export class DashboardPage implements OnInit {
   units$: Observable<Unit[]>;
+  unitsAmount: number;
   isNewUnitFormDisplayable: boolean = false;
 
   constructor(
-    private  dbService: DBService,
+    private dbService: DBService,
     private modalCtrl: ModalController
   ) {}
 
   ngOnInit() {
     this.units$ = this.dbService.getUnits();
+    this.units$.subscribe(resp => {
+      this.unitsAmount = resp.length;
+    });
   }
 
   displayNewUnitForm() {
@@ -36,19 +40,16 @@ export class DashboardPage implements OnInit {
   async openUnitModal(unit: Unit) {
     const modal = await this.modalCtrl.create({
       component: UnitModalComponent,
-      componentProps: {unit}
+      componentProps: { unit }
     });
     return await modal.present();
   }
 
   onUnitCreate(newUnitForm: NgForm) {
-    const {title} = newUnitForm.value;
+    const { title } = newUnitForm.value;
     this.dbService.addUnit(title).then(() => {
       this.hideNewUnitForm();
     });
   }
-
-  onUnitEdit(unit: Unit) {
-  }
-
+  
 }
