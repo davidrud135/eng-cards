196b6ce55598a5e3ed89ee429e2c08da52915982
David RJ <david.rud135@gmail.com>
2019-07-30 20:48:57 +0300

    Added anonymous guard to Sign In and Sign Up pages

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,10 +7,11 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { SignInPage } from './sign-in/sign-in.page';
 import { SignUpPage } from './sign-up/sign-up.page';
+import { AnonymousGuard } from './anonymous.guard';
 
 const routes: Routes = [
-  { path: 'sign-in', component: SignInPage },
-  { path: 'sign-up', component: SignUpPage },
+  { path: 'sign-in', component: SignInPage, canActivate: [AnonymousGuard] },
+  { path: 'sign-up', component: SignUpPage, canActivate: [AnonymousGuard] },
 ];
 
 @NgModule({
