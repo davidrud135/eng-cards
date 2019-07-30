87427ca9e828ecf7fafde794e2da872414f2c67b
David RJ <david.rud135@gmail.com>
2019-07-30 20:25:34 +0300

    Added auth guard to Learn page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [AuthGuard] },
-  { path: 'learn', loadChildren: './learn/learn.module#LearnPageModule' },
+  { path: 'learn', loadChildren: './learn/learn.module#LearnPageModule', canActivate: [AuthGuard] },
 ];
 
 @NgModule({
