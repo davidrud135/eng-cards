e597b7b61b006150f433b881c4e21ec04163873e
David RJ <david.rud135@gmail.com>
2019-08-02 17:43:26 +0300

    Refactored service

--- src/app/shared/database.service.ts
+++ src/app/shared/database.service.ts
@@ -1,90 +1,98 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  DocumentChangeAction,
+  AngularFirestoreCollection,
+  DocumentData,
+} from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { AuthService } from '../auth/auth.service';
 import { Unit } from './models/unit.model';
 import { Card } from './models/card.model';
 
 @Injectable({ providedIn: 'root' })
 export class DBService {
-  private userId: string;
+  private userUnitsCollection: AngularFirestoreCollection<DocumentData>;
 
-  constructor(private authService: AuthService, private afs: AngularFirestore) {
-    this.userId = this.authService.getUserId();
+  constructor(private afs: AngularFirestore) {}
+
+  public setCurrentUserCollection(userId: string): void {
+    this.userUnitsCollection = this.afs.collection(`users/${userId}/units`);
   }
 
-  getUnits(): Observable<Unit[]> {
-    return this.afs
-      .collection(`users/${this.userId}/units`)
+  public getUnits(): Observable<Unit[]> {
+    return this.userUnitsCollection.snapshotChanges().pipe(map(this.getDocsDataWithId));
+  }
+
+  public getUnitCards(unitId: string): Observable<Card[]> {
+    return this.userUnitsCollection
+      .doc(unitId)
+      .collection('cards')
       .snapshotChanges()
-      .pipe(
-        map((snapshot: any) => {
-          return snapshot.map((unitFirebaseResp: { payload: any }) => {
-            const data = unitFirebaseResp.payload.doc.data();
-            const id = unitFirebaseResp.payload.doc.id;
-            return { id, ...data };
-          });
-        }),
-      );
+      .pipe(map(this.getDocsDataWithId));
   }
 
-  getUnitCards(unitId: string): Observable<Card[]> {
-    return this.afs
-      .collection(`users/${this.userId}/units/${unitId}/cards`)
-      .snapshotChanges()
-      .pipe(
-        map((snapshot: any) => {
-          return snapshot.map((cardFirebaseResp: { payload: any }) => {
-            const data = cardFirebaseResp.payload.doc.data();
-            const id = cardFirebaseResp.payload.doc.id;
-            return { id, ...data };
-          });
-        }),
-      );
+  private getDocsDataWithId(collectionActions: DocumentChangeAction<any>[]): any[] {
+    return collectionActions.map((docAction: DocumentChangeAction<any>) => {
+      const doc = docAction.payload.doc;
+      const id = doc.id;
+      const data = doc.data();
+      return { id, ...data };
+    });
   }
 
-  addUnit(unitTitle: string): Promise<firebase.firestore.DocumentReference> {
-    return this.afs.collection(`users/${this.userId}/units`).add({
+  public addUnit(unitTitle: string): Promise<firebase.firestore.DocumentReference> {
+    return this.userUnitsCollection.add({
       title: unitTitle,
     });
   }
 
-  editUnit(unitId: string, newTitle: string): Promise<void> {
-    return this.afs.doc(`users/${this.userId}/units/${unitId}`).update({
+  public editUnit(unitId: string, newTitle: string): Promise<void> {
+    return this.userUnitsCollection.doc(unitId).update({
       title: newTitle,
     });
   }
 
-  removeUnit(unitId: string): Promise<void> {
-    return this.afs.doc(`users/${this.userId}/units/${unitId}`).delete();
+  public removeUnit(unitId: string): Promise<void> {
+    return this.userUnitsCollection.doc(unitId).delete();
   }
 
-  addCardToUnit(
+  public addCardToUnit(
     unitId: string,
     word: string,
     translation: string,
   ): Promise<firebase.firestore.DocumentReference> {
-    return this.afs.collection(`users/${this.userId}/units/${unitId}/cards`).add({
-      wordText: word,
-      wordTranslation: translation,
-    });
+    return this.userUnitsCollection
+      .doc(unitId)
+      .collection('cards')
+      .add({
+        wordText: word,
+        wordTranslation: translation,
+      });
   }
 
-  editCardInUnit(
+  public editCardInUnit(
     unitId: string,
     cardId: string,
     newWord: string,
     newTranslation: string,
   ): Promise<void> {
-    return this.afs.doc(`users/${this.userId}/units/${unitId}/cards/${cardId}`).update({
-      wordText: newWord,
-      wordTranslation: newTranslation,
-    });
+    return this.userUnitsCollection
+      .doc(unitId)
+      .collection('cards')
+      .doc(cardId)
+      .update({
+        wordText: newWord,
+        wordTranslation: newTranslation,
+      });
   }
 
-  removeCardFromUnit(unitId: string, cardId: string): Promise<void> {
-    return this.afs.doc(`users/${this.userId}/units/${unitId}/cards/${cardId}`).delete();
+  public removeCardFromUnit(unitId: string, cardId: string): Promise<void> {
+    return this.userUnitsCollection
+      .doc(unitId)
+      .collection('cards')
+      .doc(cardId)
+      .delete();
   }
 }
